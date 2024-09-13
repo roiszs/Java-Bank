ea8e19dae05bcac723fd551b23f563cfe71acf1e
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-09-13 17:26:36 -0500

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            int result = 10 / 0; //Genera una exepcion
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir por cero!");
+        }
     }
 }
